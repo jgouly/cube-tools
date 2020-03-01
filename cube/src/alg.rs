c573737f3e8511ead43bff83b0a4949c6513dc13
Joey Gouly <joey.gouly@gmail.com>
2020-03-01 11:37:05 +0000

    [cube] Add Alg::expand to expand commutators / conjugates

--- cube/src/alg.rs
+++ cube/src/alg.rs
@@ -4,7 +4,7 @@ mod parser;
 pub use parser::parse_alg;
 
 /// The amount of a move.
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, Debug)]
 #[cfg_attr(test, derive(PartialEq))]
 pub enum Amount {
   Single,
@@ -12,11 +12,20 @@ pub enum Amount {
 }
 
 /// The direction of a move.
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, Debug)]
 #[cfg_attr(test, derive(PartialEq))]
 pub enum Direction {
   Clockwise,
   AntiClockwise,
+}
+
+impl Direction {
+  fn invert(self) -> Direction {
+    match self {
+      Direction::Clockwise => Direction::AntiClockwise,
+      Direction::AntiClockwise => Direction::Clockwise,
+    }
+  }
 }
 
 /// Represents a move of the cube.
@@ -26,11 +35,73 @@ pub enum Move {
   Face(Face, Amount, Direction),
 }
 
+impl Move {
+  fn invert(&self) -> Move {
+    match self {
+      Move::Face(f, amt, dir) => Move::Face(*f, *amt, dir.invert()),
+    }
+  }
+}
+
 #[derive(Clone, Debug)]
 #[cfg_attr(test, derive(PartialEq))]
 pub enum Alg {
-  None,
   Seq(Vec<Move>),
   Comm(Box<Alg>, Box<Alg>),
   Conj(Box<Alg>, Box<Alg>),
+}
+
+impl Alg {
+  fn seq_as_vec(self) -> Vec<Move> {
+    if let Alg::Seq(moves) = self {
+      moves
+    } else {
+      unreachable!()
+    }
+  }
+
+  /// Expand an Alg into an Alg::Seq.
+  pub fn expand(&self) -> Alg {
+    let mut moves = vec![];
+    match self {
+      Alg::Seq(inner) => moves = inner.clone(),
+      Alg::Comm(a, b) => {
+        let a = a.expand().seq_as_vec();
+        let b = b.expand().seq_as_vec();
+        moves.extend(a.clone());
+        moves.extend(b.clone());
+        moves.extend(a.iter().rev().map(|m| m.invert()));
+        moves.extend(b.iter().rev().map(|m| m.invert()));
+      }
+      Alg::Conj(a, b) => {
+        let a = a.expand().seq_as_vec();
+        let b = b.expand().seq_as_vec();
+        moves.extend(a.clone());
+        moves.extend(b);
+        moves.extend(a.iter().rev().map(|m| m.invert()));
+      }
+    }
+    Alg::Seq(moves)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use parser::parse_alg;
+
+  #[test]
+  fn expand() {
+    let comm = parse_alg("[R, U]").unwrap();
+    assert_eq!(parse_alg("R U R' U'").unwrap(), comm.expand());
+
+    let conj = parse_alg("[R: U]").unwrap();
+    assert_eq!(parse_alg("R U R'").unwrap(), conj.expand());
+
+    let alg = parse_alg("[D: [R, U]]").unwrap();
+    assert_eq!(parse_alg("D R U R' U' D'").unwrap(), alg.expand());
+
+    let alg = parse_alg("[R': [R, U]]").unwrap();
+    assert_eq!(parse_alg("R' R U R' U' R").unwrap(), alg.expand());
+  }
 }
