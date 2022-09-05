6cc425c058cfd1df789156b0885a1b3f798acacd
Joey Gouly <joey.gouly@gmail.com>
2022-09-05 16:45:34 +0100

    [kociemba] add Coordinate trait

--- kociemba/src/lib.rs
+++ kociemba/src/lib.rs
@@ -1,1 +1,14 @@
 mod piece_cube;
+
+use piece_cube::PieceCube;
+
+pub(crate) trait Coord {
+  // Number of elements in `Coord`'s transition table.
+  const NUM_ELEMS: usize;
+
+  // Modify `Cube` to have the given coordinate.
+  fn set_coord(cube: &mut PieceCube, coord: usize);
+
+  // Get the coordinate for a given `Cube`.
+  fn get_coord(cube: &PieceCube) -> usize;
+}
